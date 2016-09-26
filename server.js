4a61fb983f78198c6a51f2709fb12af8fee378df
sreedevi2016 <sreedevi2016@users.noreply.github.com>
2016-09-26 21:09:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.get('/article-three', function(req,res){
 });
 
 app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
